bad50be30b123e22b675f85de618de43d3559063
drimag <rafaelvedimagiba@gmail.com>
2026-02-08 20:49:28 +0800

    new scan styles

--- ui/src/components/Scan/PhotoUpload.jsx
+++ ui/src/components/Scan/PhotoUpload.jsx
@@ -1,21 +1,34 @@
-import React, { useState, useRef } from "react";
-export default function PhotoUpload({ title, preview, setPreview }) {
+import React, { forwardRef } from "react";
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    if (file && file.type.startsWith("image/")) {
-      setPreview(URL.createObjectURL(file));
-    }
-  };
-
+const PhotoUpload = forwardRef(({ preview, setPreview, title }, ref) => {
   return (
     <div className="scan-field">
       <label>{title}</label>
-      <input type="file" accept="image/*" onChange={handleFileChange} />
+      
+      <input
+        type="file"
+        accept="image/*"
+        ref={ref}
+        style={{ display: "none" }}
+        onChange={(e) => {
+          if (e.target.files[0]) {
+            setPreview(URL.createObjectURL(e.target.files[0]));
+          }
+        }}
+      />
 
-      {preview && (
-        <img src={preview} alt="Selected preview" className="scan-preview-img" />
-      )}
+      <div 
+        className="custom-upload-box" 
+        onClick={() => ref.current.click()}
+      >
+        {preview ? (
+          <img src={preview} alt="Preview" className="preview-small" />
+        ) : (
+          <span>Upload Photo</span>
+        )}
+      </div>
     </div>
   );
-}
+});
+
+export default PhotoUpload;
