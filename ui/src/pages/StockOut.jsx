25b2190a1680ff5a2446de01ff86ec764f63eade
drimag <rafaelvedimagiba@gmail.com>
2026-02-09 13:01:08 +0800

    scanning popout

--- ui/src/pages/StockOut.jsx
+++ ui/src/pages/StockOut.jsx
@@ -5,7 +5,6 @@ import ScanResult from "../components/Scan/ScanResult";
 import GenericSelect from "../components/Scan/GenericSelect";
 import "../styles/scan.css";
 import WaybillResult from "../components/Scan/WaybillResult";
-import Scan from "./Scan";
 
 export default function StockOut() {
   const wayscanRef = useRef(null);
@@ -25,15 +24,37 @@ export default function StockOut() {
   const [driver, setDriver] = useState("");
   const [truck, setTruck] = useState("");
   const [quantity, setQuantity] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [engine, setEngine] = useState("");
+  const [frame, setFrame] = useState("");
+  const [model, setModel] = useState("");
+  const [color, setColor] = useState("");
 
   const driverList = ["Driver A", "Driver B", "Driver C"];
   const truckList = ["Truck A", "Truck B", "Truck C"];
   const warehouseList = ["Warehouse A", "Warehouse B", "Warehouse C"];
 
   const handleSubmit = () => {
-    if (!waybill || !warehouse) return;
     setSubmitted(true);
   };
+
+  const handleConfirm = () => {
+    setShowModal(true);
+  };
+
+  const handleEnd = () => {
+    setShowModal(false);
+    setSubmitted(false);
+  };
+
+  const handleNext = () => {
+    setEngine("");
+    setFrame("");
+    setModel("");
+    setColor("");
+  };
+
+  const emptyFunc = () => {};
 
   const focusNext = (nextRef) => {
     const element = nextRef.current;
@@ -53,105 +74,151 @@ export default function StockOut() {
 
   return (
     <div className="page-centered page">
-      <h1 className="page-title">Stock Out</h1>
+      {!submitted ? (
+        <>
+          <h1 className="page-title">Stock Out</h1>
 
-      <ScanInput
-        vin={waybillname}
-        setVin={setWaybillName}
-        title={"Name Waybill"}
-        onKeyDown={(e) => e.key === "Enter" && focusNext(wayscanRef)}
-        placeholder={"Name New Waybill"}
-      />
+          <ScanInput
+            vin={waybillname}
+            setVin={setWaybillName}
+            title={"Name Waybill"}
+            onKeyDown={(e) => e.key === "Enter" && focusNext(wayscanRef)}
+            placeholder={"Name New Waybill"}
+          />
 
-      <ScanInput
-        ref={wayscanRef}
-        vin={waybillcode}
-        setVin={setWaybillCode}
-        title={"Scan Waybill"}
-        onKeyDown={(e) => e.key === "Enter" && focusNext(originRef)}
-        placeholder={"Scan New Waybill"}
-      />
+          <ScanInput
+            ref={wayscanRef}
+            vin={waybillcode}
+            setVin={setWaybillCode}
+            title={"Scan Waybill"}
+            onKeyDown={(e) => e.key === "Enter" && focusNext(originRef)}
+            placeholder={"Scan New Waybill"}
+          />
 
-      <div className="warehouse-row">
-        <GenericSelect
-          ref={originRef}
-          selected={origin}
-          setSelected={(val) => {
-            setOrigin(val);
-            focusNext(destRef);
-          }}
-          title={"Origin"}
-          options={warehouseList}
-          placeholder={"Select Warehouse"}
-        />
+          <div className="warehouse-row">
+            <GenericSelect
+              ref={originRef}
+              selected={origin}
+              setSelected={(val) => {
+                setOrigin(val);
+                focusNext(destRef);
+              }}
+              title={"Origin"}
+              options={warehouseList}
+              placeholder={"Select Warehouse"}
+            />
 
-        <GenericSelect
-          ref={destRef}
-          selected={destination}
-          setSelected={(val) => {
-            setDestination(val);
-            focusNext(driverRef);
-          }}
-          title={"Destination"}
-          options={warehouseList}
-          placeholder={"Select Warehouse"}
-        />
-      </div>
+            <GenericSelect
+              ref={destRef}
+              selected={destination}
+              setSelected={(val) => {
+                setDestination(val);
+                focusNext(driverRef);
+              }}
+              title={"Destination"}
+              options={warehouseList}
+              placeholder={"Select Warehouse"}
+            />
+          </div>
 
-      <GenericSelect
-        ref={driverRef}
-        selected={driver}
-        setSelected={(val) => {
-          setDriver(val);
-          focusNext(truckRef);
-        }}
-        title={"Driver"}
-        options={driverList}
-        placeholder={"Select Driver"}
-      />
-      <GenericSelect
-        ref={truckRef}
-        selected={truck}
-        setSelected={(val) => {
-          setTruck(val);
-          focusNext(qtyRef);
-        }}
-        title={"Truck"}
-        options={truckList}
-        placeholder={"Select Truck"}
-      />
+          <GenericSelect
+            ref={driverRef}
+            selected={driver}
+            setSelected={(val) => {
+              setDriver(val);
+              focusNext(truckRef);
+            }}
+            title={"Driver"}
+            options={driverList}
+            placeholder={"Select Driver"}
+          />
+          <GenericSelect
+            ref={truckRef}
+            selected={truck}
+            setSelected={(val) => {
+              setTruck(val);
+              focusNext(qtyRef);
+            }}
+            title={"Truck"}
+            options={truckList}
+            placeholder={"Select Truck"}
+          />
 
-      <ScanInput
-        ref={qtyRef}
-        vin={quantity}
-        setVin={setQuantity}
-        title="Quantity"
-        onKeyDown={(e) => e.key === "Enter" && focusNext(photoRef)}
-        placeholder={"Enter Quantity"}
-      />
-      <PhotoUpload
-        ref={photoRef}
-        title={"Photo"}
-        preview={preview}
-        setPreview={setPreview}
-      />
+          <ScanInput
+            ref={qtyRef}
+            vin={quantity}
+            setVin={setQuantity}
+            title="Quantity"
+            onKeyDown={(e) => e.key === "Enter" && focusNext(photoRef)}
+            placeholder={"Enter Quantity"}
+          />
+          <PhotoUpload
+            ref={photoRef}
+            title={"Photo"}
+            preview={preview}
+            setPreview={setPreview}
+          />
 
-      <button className="primary-btn" onClick={handleSubmit}>
-        Confirm
-      </button>
+          <button className="primary-btn" onClick={handleSubmit}>
+            Proceed
+          </button>
+        </>
+      ) : (
+        <>
+          <WaybillResult
+            waybill={waybillname}
+            driver={"DriverName"}
+            truck={"TruckID"}
+            time={"currenttime"}
+            status={"Active"}
+            origin={"Warehouse"}
+            destination={"Destination"}
+            inout={"Incoming"}
+            photo={preview}
+            handleSubmit={handleConfirm}
+          />
+        </>
+      )}
 
-      {submitted && (
-        <WaybillResult
-          waybill={waybill}
-          driver={"DriverName"}
-          truck={"TruckID"}
-          time={"currenttime"}
-          status={"Active"}
-          origin={"Warehouse"}
-          destination={warehouse}
-          inout={"Incoming"}
-          photo={preview}
-        />
+      {/* The Popout Overlay */}
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <h1 className="page-title">Scan Next Unit</h1>
+            <ScanInput
+              vin={engine}
+              setVin={setEngine}
+              title={"Engine"}
+              placeholder={"Enter Unit Engine"}
+            />
+            <ScanInput
+              vin={frame}
+              setVin={setFrame}
+              title={"Frame"}
+              placeholder={"Enter Unit Frame"}
+            />
+            <ScanInput
+              vin={model}
+              setVin={setModel}
+              title={"Model"}
+              placeholder={"Enter Unit Model"}
+            />
+            <ScanInput
+              vin={color}
+              setVin={setColor}
+              title={"Color"}
+              placeholder={"Enter Unit Color"}
+            />
+            <div className="warehouse-row">
+              <button className="primary-btn" onClick={handleNext}>
+                Next Unit
+              </button>
+              <button className="primary-btn" onClick={handleEnd}>
+                Finish
+              </button>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
