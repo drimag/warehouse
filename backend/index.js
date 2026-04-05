b3430df54e75b11c263f363a393c65fd0a46ebb1
drimag <rafaelvedimagiba@gmail.com>
2026-04-05 23:44:00 +0800

    update allowed origins (vercel) and methods in cors policy

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const unitRoutes = require('./routes/unitRoutes');
 
 const allowedOrigins = [
   'http://localhost:5173', 
-  'https://warehouse-drab.vercel.app/' // Add your Vercel URL here
+  'https://warehouse-drab.vercel.app' // Add your Vercel URL here
 ];
 
 const app = express();
@@ -13,12 +13,16 @@ app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (like mobile apps or curl)
     if (!origin) return callback(null, true);
+    
     if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('CORS policy violation'), false);
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
     }
     return callback(null, true);
-  }
-})); // Critical: Allows React (port 3000) to talk to Node (port 5000)
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));// Critical: Allows React (port 3000) to talk to Node (port 5000)
 app.use(express.json());
 
 app.use('/api/waybills', waybillRoutes);
